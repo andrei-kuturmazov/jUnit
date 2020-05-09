f2b7aa0625fbcb5699afabb6a2e9b7fa54d12cd0
andrei_kuturmazov <andrei.kuturmazov@gmail.com>
2020-05-09 21:46:49 +0300

    using parametrization

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,17 +1,35 @@
+import lombok.AllArgsConstructor;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 import static org.junit.Assert.*;
 
+@AllArgsConstructor
+@RunWith(Parameterized.class)
 public class CalculatorTest {
 
-    @Test
-    public void testSum() {
-        int result = new Calculator().sum(3, 4);
-        assertEquals(7, result);
+    int first, second, expected;
+
+    @Parameterized.Parameters
+    public static Collection numbers() {
+        return Arrays.asList(new Object [][] {{1, 2, 3}, {2, 9, 11}, {3, 7, 4}});
     }
 
+    @Test(timeout = 1000)
+    public void testSum() {
+        int result = new Calculator().sum(first, second);
+        assertEquals(expected, result);
+    }
+
+    @Ignore
     @Test(expected = ArithmeticException.class)
     public void testDivision() {
-        int result = new Calculator().division(4, 0);
-        assertEquals(5, result);
+        int result = new Calculator().division(first, second);
+        assertEquals(expected, result);
     }
 }
